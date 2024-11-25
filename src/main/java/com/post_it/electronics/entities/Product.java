22ff93012524dc4fe6ac5115e0aafbb4db0e638f
asif-kamal <asif.kamal1489@gmail.com>
2024-11-25 13:16:59 -0500

    added fields and JPA/Lombok annotations to represent a product object in the database

--- src/main/java/com/post_it/electronics/entities/Product.java
+++ src/main/java/com/post_it/electronics/entities/Product.java
@@ -1,4 +1,58 @@
 package com.post_it.electronics.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
+
+    @Column
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private Boolean isNewArrival;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = new Date();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = new Date();
+    }
 }
