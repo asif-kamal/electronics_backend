0472e30f7fb923b13573cb62792fd418a1f60735
asif-kamal <asif.kamal1489@gmail.com>
2024-11-30 11:17:15 -0500

    created product repo extending JpaRepository

---
+++ src/main/java/com/post_it/electronics/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.post_it.electronics.repositories;
+
+import com.post_it.electronics.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+
+}
