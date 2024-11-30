b23ca1d866000224f12c220947675b58bbcf4e7b
asif-kamal <asif.kamal1489@gmail.com>
2024-11-30 11:16:16 -0500

    returned new ResponseEntity w/ http status, and autowired through constructor

--- src/main/java/com/post_it/electronics/controllers/ProductController.java
+++ src/main/java/com/post_it/electronics/controllers/ProductController.java
@@ -1,18 +1,30 @@
 package com.post_it.electronics.controllers;
 
 import com.post_it.electronics.dto.ProductDTO;
+import com.post_it.electronics.entities.Product;
+import com.post_it.electronics.services.ProductService;
+import com.post_it.electronics.services.ProductServiceImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping
-    public ResponseEntity<List<ProductDTO>> getAllProducts() {
-        return null;
+    public ResponseEntity<List<Product>> getAllProducts(UUID categoryId, UUID typeId) {
+        List<Product> productList = productService.getAllProducts(categoryId, typeId);
+        return new ResponseEntity<List<Product>>(productList, HttpStatus.OK);
     }
 
     @PostMapping
