a99d359208c86f63b4b7f256822f8c39b35b56e4
asif-kamal <asif.kamal1489@gmail.com>
2024-11-30 11:19:58 -0500

    Built ProductServiceImpl using productRepository, implementing abstract methods

---
+++ src/main/java/com/post_it/electronics/services/ProductServiceImpl.java
@@ -0,0 +1,32 @@
+package com.post_it.electronics.services;
+
+import com.post_it.electronics.dto.ProductDTO;
+import com.post_it.electronics.entities.Product;
+import com.post_it.electronics.repositories.ProductRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product addProduct(ProductDTO product) {
+
+        return null;
+    }
+
+    @Override
+    public List<Product> getAllProducts(UUID categoryId, UUID typeId) {
+        //To-do mapping of products to productDTOs
+        return productRepository.findAll();
+    }
+}
