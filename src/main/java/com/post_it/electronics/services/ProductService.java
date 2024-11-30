60d8687994bd7c766666d5fbafd7ee319f8cc8c0
asif-kamal <asif.kamal1489@gmail.com>
2024-11-30 11:18:04 -0500

    created ProductService interface w/ 2 abstract methods

---
+++ src/main/java/com/post_it/electronics/services/ProductService.java
@@ -0,0 +1,14 @@
+package com.post_it.electronics.services;
+
+import com.post_it.electronics.dto.ProductDTO;
+import com.post_it.electronics.entities.Product;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ProductService {
+
+    public Product addProduct(ProductDTO product);
+
+    public List<Product> getAllProducts(UUID categoryId, UUID typeId);
+}
