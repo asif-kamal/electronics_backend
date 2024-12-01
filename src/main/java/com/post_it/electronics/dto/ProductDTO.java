27234709f7bcd92d89e7d547da1d7a37435e7d7c
asif-kamal <asif.kamal1489@gmail.com>
2024-12-01 11:50:40 -0500

    Added lombok anno and fields for ProductDTO

--- src/main/java/com/post_it/electronics/dto/ProductDTO.java
+++ src/main/java/com/post_it/electronics/dto/ProductDTO.java
@@ -1,9 +1,25 @@
 package com.post_it.electronics.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductDTO {
 
     private UUID id;
     private String name;
+    private String description;
+    private BigDecimal price;
+    private String brand;
+    private boolean isNewArrival;
+    private UUID categoryId;
+    private UUID categoryTypeId;
 }
