a2c985f8a061d95990ef427259b2d1d6584426f5
asif-kamal <asif.kamal1489@gmail.com>
2024-12-04 13:08:59 -0500

    Built mapToCategoryTypesList method to map dto to new category

---
+++ src/main/java/com/post_it/electronics/services/CategoryService.java
@@ -0,0 +1,56 @@
+package com.post_it.electronics.services;
+
+import com.post_it.electronics.dto.CategoryDTO;
+import com.post_it.electronics.dto.CategoryTypeDTO;
+import com.post_it.electronics.entities.Category;
+import com.post_it.electronics.entities.CategoryType;
+import com.post_it.electronics.repositories.CategoryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor // instead of using field injection
+public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+
+    public Category getCategory(UUID categoryId) {
+        Optional<Category> category = categoryRepository.findById(categoryId);
+
+        return category.orElse(null);
+    }
+
+    public Category createCategory(CategoryDTO categoryDTO) {
+
+    }
+
+    private Category mapToEntity(CategoryDTO categoryDTO) {
+        Category category = Category.builder()
+                .code(categoryDTO.getCode())
+                .name(categoryDTO.getName())
+                .description(categoryDTO.getDescription())
+                .categoryTypeList(mapToCategoryTypesList(categoryDTO.getCategoryTypeDTOList()))
+                .build();
+    }
+
+    private List<CategoryType> mapToCategoryTypesList(List<CategoryTypeDTO> categoryTypeDTOList) {
+        return categoryTypeDTOList.stream().map(categoryTypeDTO -> {
+            CategoryType categoryType = new CategoryType();
+            categoryType.setCode(categoryTypeDTO.getCode());
+            categoryType.setName(categoryTypeDTO.getName());
+            categoryType.setDescription(categoryType.getDescription());
+            return categoryType;
+        }).collect(Collectors.toList());
+    }
+
+    private Object mapToCategoryType(CategoryTypeDTO categoryTypeDTO) {
+
+
+    }
+}
