f7600d9c36490949ec4cfbadd03c4c38a6b5298e
asif-kamal <asif.kamal1489@gmail.com>
2024-12-20 16:39:24 -0500

    Added RequiredArgsConstructor for controller to autowire productService instance.

--- src/main/java/com/post_it/electronics/controllers/ProductController.java
+++ src/main/java/com/post_it/electronics/controllers/ProductController.java
@@ -4,6 +4,7 @@ import com.post_it.electronics.dto.ProductDTO;
 import com.post_it.electronics.entities.Product;
 import com.post_it.electronics.services.ProductService;
 import com.post_it.electronics.services.ProductServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,13 +14,11 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
 
-    private ProductService productService;
+    private final ProductService productService;
 
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
 
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts(UUID categoryId, UUID typeId) {
