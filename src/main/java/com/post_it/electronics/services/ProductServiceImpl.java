56c158c209a70bb3b518b007f3f52b7a9f432c96
asif-kamal <asif.kamal1489@gmail.com>
2024-12-21 10:29:13 -0500

    Set categoryType of product to categoryType of matching IDs inside mapToProductEntity

--- src/main/java/com/post_it/electronics/services/ProductServiceImpl.java
+++ src/main/java/com/post_it/electronics/services/ProductServiceImpl.java
@@ -1,27 +1,30 @@
 package com.post_it.electronics.services;
 
 import com.post_it.electronics.dto.ProductDTO;
+import com.post_it.electronics.entities.Category;
+import com.post_it.electronics.entities.CategoryType;
 import com.post_it.electronics.entities.Product;
 import com.post_it.electronics.repositories.ProductRepository;
-import org.springframework.stereotype.Repository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
+    private final CategoryService categoryService;
 
-    public ProductServiceImpl(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
 
     @Override
-    public Product addProduct(ProductDTO product) {
+    public Product addProduct(ProductDTO productDTO) {
+        Product product = mapToProductEntity(productDTO);
 
-        return null;
+        return productRepository.save(product);
     }
 
     @Override
@@ -30,7 +33,7 @@ public class ProductServiceImpl implements ProductService {
         return productRepository.findAll();
     }
 
-    private Product createProduct(ProductDTO productDTO) {
+    private Product mapToProductEntity(ProductDTO productDTO) {
         Product product = new Product();
         product.setName(productDTO.getName());
         product.setDescription(productDTO.getDescription());
@@ -38,6 +41,17 @@ public class ProductServiceImpl implements ProductService {
         product.setIsNewArrival(productDTO.isNewArrival());
         product.setPrice(productDTO.getPrice());
 
+        Category category = categoryService.getCategory(productDTO.getCategoryId());
+        if (null != category) {
+            product.setCategory(category);
+            UUID categoryTypeId = productDTO.getCategoryTypeId();
+            Optional<CategoryType> categoryTypeOptional = category.getCategoryTypeList().stream()
+                    .filter(categoryType1 -> categoryType1.getId().equals(categoryTypeId)).findFirst();
+            if (categoryTypeOptional.isPresent()) {
+                CategoryType categoryType = categoryTypeOptional.get();
+                product.setCategoryType(categoryType);
+            }
+        }
         return product;
     }
 }
