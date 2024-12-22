a26a605ca9ba0918418671ce70b2b6314fe3251b
asif-kamal <asif.kamal1489@gmail.com>
2024-12-22 11:37:27 -0500

    Added 2 mapping methods to map DTOs to entities

--- src/main/java/com/post_it/electronics/services/ProductServiceImpl.java
+++ src/main/java/com/post_it/electronics/services/ProductServiceImpl.java
@@ -1,9 +1,9 @@
 package com.post_it.electronics.services;
 
 import com.post_it.electronics.dto.ProductDTO;
-import com.post_it.electronics.entities.Category;
-import com.post_it.electronics.entities.CategoryType;
-import com.post_it.electronics.entities.Product;
+import com.post_it.electronics.dto.ProductResourceDTO;
+import com.post_it.electronics.dto.ProductVariantDTO;
+import com.post_it.electronics.entities.*;
 import com.post_it.electronics.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -52,6 +53,36 @@ public class ProductServiceImpl implements ProductService {
                 product.setCategoryType(categoryType);
             }
         }
+        if (null != productDTO.getProductVariantDTOList()) {
+            product.setProductVariantList(mapToProductVariant(productDTO.getProductVariantDTOList(), product));
+        }
+        if (null != productDTO.getProductResourceDTOList()) {
+            product.setResourceList(mapToProductResources(productDTO.getProductResourceDTOList(), product));
+        }
+
         return product;
+    }
+
+    private List<Resource> mapToProductResources(List<ProductResourceDTO> productResourceDTOList, Product product) {
+        return productResourceDTOList.stream().map(productResourceDTO -> {
+            Resource resource = new Resource();
+            resource.setName(productResourceDTO.getName());
+            resource.setType(productResourceDTO.getType());
+            resource.setUrl(productResourceDTO.getUrl());
+            resource.setIsPrimary(productResourceDTO.isPrimary());
+            resource.setProduct(product);
+            return resource;
+        }).collect(Collectors.toList());
+    }
+
+    private List<ProductVariant> mapToProductVariant(List<ProductVariantDTO> productVariantDTOList, Product product) {
+        return productVariantDTOList.stream().map(productVariantDTO -> {
+            ProductVariant productVariant = new ProductVariant();
+            productVariant.setColor(productVariantDTO.getColor());
+            productVariant.setSize(productVariantDTO.getSize());
+            productVariant.setStockQuantity(productVariantDTO.getStockQuantity());
+            productVariant.setProduct(product);
+            return productVariant;
+        }).collect(Collectors.toList());
     }
 }
